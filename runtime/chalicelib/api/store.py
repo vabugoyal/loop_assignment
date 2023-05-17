3c117795387799c0dcae97ffca3897736d3bfc3e
vaibhav-ti <vaibhav.goyal@trilogy.com>
2023-05-18 00:46:10 +0530

    wrote report logic for last n days

--- runtime/chalicelib/api/store.py
+++ runtime/chalicelib/api/store.py
@@ -1,11 +1,11 @@
 import logging
-from datetime import datetime
-import time, pytz
+
 
 from chalice import Blueprint, BadRequestError, UnauthorizedError
 
 from ..constants import *
 from ..models.store import StoreStatus, StoreBusinessHours, StoreTimeZone
+from ..services.store import generate_report_last_n_days
 
 store_routes = Blueprint('store')
 logger = logging.getLogger(__name__)
@@ -18,83 +18,12 @@ def dummy():
     return {"status": "working"}
 
 
-def in_between(now, start, end):
-    if start <= end:
-        return start <= now < end
-    else: # over midnight e.g., 23:30-04:15
-        return start <= now or now < end
-
-
-
-
 @store_routes.route('/{id}/report', methods=['POST'], cors=True)
 def generate_report(id):
     """Given a store id generate a report of the store for last week."""
     """
     Fetch all the polls of this store done in last week.
     """
-
-    # find the start and end time in local time format
-    schedules = StoreBusinessHours.where(store_id=id)
-    interval = dict()
-
-    total_polls = 0
-    active_polls = 0
-    ideal_polls = 0
-
-    for schedule in schedules:
-        interval[schedule.dayOfWeek] = schedule
-        hours = interval[schedule.dayOfWeek].start_time_local.hour - interval[schedule.dayOfWeek].end_time_local.hour
-        ideal_polls += (hours + 24) % 24
-
-    time_zone = StoreTimeZone.find_or_fail(id).time_zone
-
-    polls = StoreStatus.where(store_id=id)
-    # take the poll with current timestamp as max timestamp
-
-
-
-    max_time_stamp = None
-    for poll in polls.all():
-        if not max_time_stamp or poll.timestamp > max_time_stamp: max_time_stamp = poll.timestamp
-
-    current_timestamp = datetime.now()
-
-    # shift all the timestamps
-    for poll in polls.all():
-        poll.timestamp = poll.timestamp + (current_timestamp - max_time_stamp)
-
-    for poll in polls.all():
-        # find if timestamp belongs to previous week
-        if (current_timestamp - poll.timestamp).days < 7:
-            weekday = poll.timestamp.weekday()
-
-            local_timezone = pytz.timezone(time_zone)
-            local_time = poll.timestamp.astimezone(local_timezone).time()
-
-            if in_between(local_time, interval[weekday].start_time_local, interval[weekday].end_time_local):
-                total_polls += 1
-                active_polls += poll.status == 'active'
-
-    print(total_polls, active_polls, ideal_polls)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    last_week_report = generate_report_last_n_days(id, 7)
+    last_day_report = generate_report_last_n_days(id, 1)
+    return last_week_report, last_day_report
---
+++ runtime/chalicelib/services/store.py
@@ -0,0 +1,98 @@
+import time
+
+from datetime import datetime, timedelta, time
+
+from ..models.store import StoreStatus, StoreTimeZone, StoreBusinessHours
+from .date_and_time import in_between, convert_to_local_time
+
+
+def shift_to_current_time_stamp(polls):
+    """
+    shift the static poll data based on current time stamp
+    """
+    # shifting the data to present timestamp
+    max_time_stamp = None
+    for poll in polls.all():
+        if not max_time_stamp or poll.timestamp > max_time_stamp: max_time_stamp = poll.timestamp
+
+    current_timestamp = datetime.now()
+
+    # shift all the timestamps
+    for poll in polls.all():
+        poll.timestamp = poll.timestamp + (current_timestamp - max_time_stamp)
+
+
+def get_schedule_on_day(schedules):
+    """returns a list of business hours on all days of the week"""
+    schedule_on_day = dict()
+    for schedule in schedules:
+        schedule_on_day[schedule.dayOfWeek] = schedule
+    # if we don't have a schedule for a particular day then make it 24 hours
+    for i in range(7):
+        if i not in schedule_on_day.keys():
+            schedule_on_day[i] = StoreBusinessHours()
+            schedule_on_day[i].start_time_local = time(0, 0, 0)
+            schedule_on_day[i].end_time_local = time(23, 59, 59)
+
+    return schedule_on_day
+
+
+def get_needed_polls_last_n_days(n, schedules):
+    """
+    returns number of business hours of the store in last n days
+    i.e the number of needed polls
+    """
+    schedule_on_day = get_schedule_on_day(schedules)
+    needed_polls = 0
+    current_timestamp = datetime.now()
+    moving_timestamp = current_timestamp
+
+    while (current_timestamp - moving_timestamp).days < n:
+        weekday = moving_timestamp.weekday()
+        hours = schedule_on_day[weekday].end_time_local.hour - schedule_on_day[weekday].start_time_local.hour
+        needed_polls += (hours + 24) % 24
+        moving_timestamp -= timedelta(days=1)
+
+    return needed_polls
+
+
+def get_last_n_days_analysis(id, n):
+    """
+    Given store id returns the number of uptime and downtime
+    hours within the service hours of the store in previous n days.
+    """
+    schedules = StoreBusinessHours.where(store_id=id)
+    time_zone = StoreTimeZone.find_or_fail(id).time_zone
+    polls = StoreStatus.where(store_id=id)
+
+    # schedule_on_day on particular day of the week
+    schedule_on_day = get_schedule_on_day(schedules)
+
+    total_polls = 0     # total actual polls done by our platform
+    active_polls = 0    # times when store was active
+    needed_polls = get_needed_polls_last_n_days(n, schedules)     # total polls which could have been done
+
+    current_timestamp = datetime.now()
+
+    shift_to_current_time_stamp(polls)
+
+    for poll in polls.all():
+        if (current_timestamp - poll.timestamp).days < n:
+            weekday = poll.timestamp.weekday()
+            local_time = convert_to_local_time(poll.timestamp, time_zone)
+
+            if in_between(local_time, schedule_on_day[weekday].start_time_local, schedule_on_day[weekday].end_time_local):
+                total_polls += 1
+                active_polls += poll.status == 'active'
+
+    return active_polls, total_polls, needed_polls
+
+
+def generate_report_last_n_days(id, n):
+    active_polls, total_polls, needed_polls = get_last_n_days_analysis(id, n)
+    print(active_polls, total_polls, needed_polls)
+    uptime_hours = needed_polls * (active_polls / total_polls)
+    return {
+        "uptime": uptime_hours,
+        "downtime": needed_polls - uptime_hours
+    }
